e55335b17efd82b3878a52db9326552ed6e8ab1a
nrusev93 <115-knz@unibit.bg>
2020-06-11 10:25:23 +0300

    Create Product Files

--- objects/product.php
+++ objects/product.php
@@ -1,7 +1,6 @@
 <?php
 class Product
 {
-
     // database connection and table name
     private $conn;
     private $table_name = "products";
@@ -22,9 +21,8 @@ class Product
     }
 
     // read products
-    function read()
+    public function read()
     {
-
         // select all query
         $query = "SELECT
                     c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
@@ -36,7 +34,32 @@ class Product
         $stmt = $this->conn->prepare($query);
         // execute query
         $stmt->execute();
-
         return $stmt;
+    }
+
+    // create product
+    public function create()
+    {
+        // query to insert record
+        $query = "INSERT INTO " . $this->table_name . "
+                SET name=:name, price=:price, description=:description, category_id=:category_id, created=:created";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->price=htmlspecialchars(strip_tags($this->price));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+        $this->created=htmlspecialchars(strip_tags($this->created));
+
+        // bind values
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":created", $this->created);
+        return $stmt->execute() ? true : false;
     }
 }
